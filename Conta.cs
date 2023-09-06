229975b4b7baa8e71cfd2158c7ee2775b3a70156
Samara M <141765028+MaraMarques@users.noreply.github.com>
2023-09-06 16:48:30 -0300

    Add files via upload

---
+++ Conta.cs
@@ -0,0 +1,17 @@
+//Samara Machado - 202220926
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace controleContas
+{
+    class Conta
+    {
+        //private int _numero;
+        private decimal _saldo;
+        public int Numero
+        {
+            get; set;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Samara Machado - 202220926
+using System;
+
+namespace controleContas {
+    class Program {
+        static void Main(string[] args) {
+            
+            Conta conta1 = new Conta();
+            conta1.Numero = 12345678;
+            Console.WriteLine(conta1.Numero);
+        }
+    }
+}
